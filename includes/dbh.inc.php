d845053f7c1cb376ebb0230dd9bedc0ca9db2778
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-09-23 20:14:26 +0200

    Added the layout of the website.

---
+++ includes/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+include 'settings.php';
+
+$dbServername = $db_host;
+$dbUsername = $db_user;
+$dbPassword = $db_password;
+$dbName = $db_name;
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
---
+++ includes/settings.php
@@ -0,0 +1,11 @@
+<?php
+//* Website Settings *//
+
+$domain_name = 'localhost';
+$site_name = 'Library System';
+
+// Database Settings //
+$db_host = 'localhost'; //The database host example "localhost"
+$db_user = 'root'; //Username for the database
+$db_password = ''; //Password for the database
+$db_name = 'LibrarySystem'; //The name of the database
