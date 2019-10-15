16d99728db510353f8b97ded85cb78a97e4d6837
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-15 17:53:30 +0200

    Skapade alert.php

---
+++ includes/alert.php
@@ -0,0 +1,37 @@
+<?php
+$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+
+    if (strpos($url,'loanAdd=Success') == true) {
+        echo "
+        <div class='alert alert-success alert-dismissible fade show' role='alert'>
+            Lånet är nu tillagt.
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+            </button>
+        </div>
+      ";
+    }
+
+
+    if (strpos($url,'loanAdd=ERROR?reason=NotAvailable') == true) {
+        echo "
+        <div class='alert alert-danger alert-dismissible fade show' role='alert'>
+            Lånet kunde inte läggas till eftersom att det valda mediet inte är tillgänligt.
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+            </button>
+        </div>
+      ";
+    }
+
+    if (strpos($url,'loanAdd=ERROR?reason=DontExist') == true) {
+        echo "
+        <div class='alert alert-danger alert-dismissible fade show' role='alert'>
+            Lånet kunde inte läggas till eftersom att det valda mediet inte existerar.
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+            </button>
+        </div>
+      ";
+    }
+?>
