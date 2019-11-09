d5bd28031a9aca23254e9f8ed415fc1f149be42b
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-11-09 20:32:09 +0100

    Fixed typo.

--- logout.php
+++ logout.php
@@ -28,7 +28,7 @@
   <body class="text-center">
     <form class="form-signin" action = "" method = "post">
       <h1 class="h3 mb-3 font-weight-normal">Loggar ut...</h1>
-      <p class="mb-3 text-muted">Du loggas nu ut från ditt konot</p>
+      <p class="mb-3 text-muted">Du loggas nu ut från ditt konto</p>
     </form>
   </body>
 </html>
