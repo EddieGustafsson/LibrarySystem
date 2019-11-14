e4cd611973cd9069e5289bed6a9fadd948ffb6aa
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-11-14 13:57:50 +0100

    Added special titles for all pages.

--- includes/settings.php
+++ includes/settings.php
@@ -1,9 +1,31 @@
 <?php
-//* Website Settings *//
+// Website Settings //
 
 $domain_name = 'localhost';
 $site_name = 'NTI - Bibliotek';
 $login_footer = 'NTI Gymnasiet - Bibliotek &copy;2019';
+
+// Page titel //
+if(isset($_GET['page'])){
+    switch ($_GET['page']) {
+        case 'home':
+            $page_titel = "Kontrollpanel";
+            break;
+        case 'addloan':
+            $page_titel = "Gör ett lån";
+            break;
+        case 'endloan':
+            $page_titel = "Avsluta ett lån";
+            break;
+        case 'reservmedia':
+            $page_titel = "Gör en bokning";
+            break;
+        default:
+            $page_titel = "Kontrollpanel"; 
+    }
+} else {
+    include('page/firstpage.php'); 
+}
 
 // Database Settings //
 $db_host = 'localhost'; //The database host example "localhost"
--- index.php
+++ index.php
@@ -16,11 +16,12 @@ if(!isset($_SESSION['login_user'])){
     <meta name="author" content="">
     <link rel="icon" type="image/png" href="assets/img/icons/favicon.ico"/>
 
-    <title><?php echo $site_name ?></title>
+    <title><?php echo $site_name; echo " - "; echo $page_titel?></title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="http://<?php echo $domain_name ?>/LibrarySystem/assets/css/dashboard.css" rel="stylesheet">
@@ -30,9 +31,9 @@ if(!isset($_SESSION['login_user'])){
   <body>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
       <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="?page=home"><?php echo $site_name ?></a>
-        <div class="md-form mt-0">
-          <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-        </div>
+      <div class="md-form mt-0">
+        <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+      </div>
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
         <form class="form-inline my-2 my-lg-0" action="http://<?php echo $domain_name ?>/LibrarySystem/logout.php">
@@ -100,12 +101,6 @@ if(!isset($_SESSION['login_user'])){
                     <li class="nav-item">
                         <a class="nav-link" href="#">
                         <h6>Information</h6>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                        <span data-feather="clock"></span>
-                            <?php echo date("H:i");?>
                         </a>
                     </li>
                     <li class="nav-item">
@@ -186,6 +181,7 @@ if(!isset($_SESSION['login_user'])){
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.2/dist/Chart.min.js"></script>
     <script src="assets/js/loanchart.js"></script>
     <!-- Latest compiled and minified JavaScript -->
